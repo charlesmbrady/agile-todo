d601338a010eb98bb60f52d451a3c8c96f8bb84d
Charles <charlesmbrady@gmail.com>
2019-11-15 00:39:47 -0500

    fixed typo

--- server/src/models/sprint.js
+++ server/src/models/sprint.js
@@ -3,10 +3,10 @@ const Schema = mongoose.Schema;
 
 const sprintSchema = new Schema({
   user: { type: Schema.Types.ObjectId, ref: "User" },
-  active: { type: Boolean, default: false }
+  active: { type: Boolean, default: false },
   name: { type: String, required: true },
   createdDate: { type: Date, default: Date.now }, // these only update when passed something
-  lastUpdateDate: { type: Date, default: Date.now }, 
+  lastUpdateDate: { type: Date, default: Date.now },
   startDate: { type: Date }, // update this when the sprint is started
   pointProjection: { type: Number, default: 0 }, // only mess with this when starting sprint
   completedPoints: { type: Number, default: 0 }, // only mess with this when ending sprint
