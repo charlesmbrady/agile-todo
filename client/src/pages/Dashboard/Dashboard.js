8c77b50b8c71d7b5d4512a0b1d931e3449a9486b
Charles <charlesmbrady@gmail.com>
2019-11-20 23:27:56 -0500

    getting chartjs working

--- client/src/pages/Dashboard/Dashboard.js
+++ client/src/pages/Dashboard/Dashboard.js
@@ -2,25 +2,38 @@ import React, { useEffect, useState } from "react";
 import "./style.css";
 import AUTH from "../../utils/AUTH";
 import API from "../../utils/API";
-import { Line as LineChart } from "react-chartjs-2";
+import { Line } from "react-chartjs-2";
 
 export default function Dashboard({ setAuthenticated }) {
   const [user, setUser] = useState({});
-  const options = {
-    scaleShowGridLines: true,
-    scaleGridLineColor: "rgba(0,0,0,.05)",
-    scaleGridLineWidth: 1,
-    scaleShowHorizontalLines: false,
-    scaleShowVerticalLines: true,
-    bezierCurve: true,
-    bezierCurveTension: 0.4,
-    pointDot: true,
-    pointDotRadius: 4,
-    pointDotStrokeWidth: 1,
-    pointHitDetectionRadius: 20,
-    datasetStroke: true,
-    datasetStrokeWidth: 2
-  };
+  const [data, setData] = useState({
+    labels: ["1", "2", "3", "4", "5", "6"],
+    datasets: [
+      {
+        label: "Burn",
+        data: [0, 2, 2, 3, 3, 5]
+      },
+      {
+        label: "Guideline",
+        data: [0, 1, 2, 3, 4, 5]
+      }
+    ]
+  });
+  // const options = {
+  //   scaleShowGridLines: true,
+  //   scaleGridLineColor: "rgba(0,0,0,.05)",
+  //   scaleGridLineWidth: 1,
+  //   scaleShowHorizontalLines: false,
+  //   scaleShowVerticalLines: true,
+  //   bezierCurve: true,
+  //   bezierCurveTension: 0.4,
+  //   pointDot: true,
+  //   pointDotRadius: 4,
+  //   pointDotStrokeWidth: 1,
+  //   pointHitDetectionRadius: 20,
+  //   datasetStroke: true,
+  //   datasetStrokeWidth: 2
+  // };
 
   useEffect(() => {
     getUser();
@@ -49,19 +62,23 @@ export default function Dashboard({ setAuthenticated }) {
   };
   return (
     <div>
-      <h1>Dashboard</h1>
-      <button onClick={() => ping()}>Ping</button>
-      <button>Get Cookie not functional</button>
-      <LineChart
-        data={[
-          { x: 5, y: 1 },
-          { x: 3, y: 1 },
-          { x: 2, y: 3 }
-        ]}
-        options={options}
-        width="600"
-        height="250"
-      />
+      <div className="chart">
+        <Line
+          // datasets={[10, 20, 30, 40, 50, 60]}
+          // labels={["January", "February", "March", "April", "May", "June"]}
+          data={data}
+          options={{
+            responsive: true
+            // maintainAspectRatio: false,
+            // scales: {
+            //   yAxes: [{ ticks: { beginAtZero: true } }],
+            //   xAxes: [{ ticks: { beginAtZero: true } }]
+            // }
+          }}
+          // width="100px"
+          // height="100px"
+        />
+      </div>
     </div>
   );
 }
